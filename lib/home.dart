b301a0978f30a0b4103fa42e949cce394a2e0511
rinakels <oriendovalencia@gmail.com>
2024-06-25 12:09:19 +0800

    Add Buttons and Profile Pic

---
+++ lib/home.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('USER PROFILE'),
+        backgroundColor: Colors.brown[300],
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children:<Widget>[
+                Center(
+                  child: CircleAvatar(
+                    child: Image.asset('assets/browngirl.jpg'),
+                    radius: 50.0,
+                  ),
+                ),
+                Divider(height: 55.0,
+                color:Colors.brown,
+                thickness: 5.0,
+                ),
+                Row(
+                  children: [
+                    Icon(
+                        Icons.person_2,
+                        color: Colors.black,
+
+                    ),
+                    SizedBox(width: 3.0,),
+                    Text(
+                      'NAME',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.black,
+                        letterSpacing: 2.0,
+
+                      ),
+
+                    ),
+                  ],
+                ),
+                Text(
+                  'Rinalyn V.Oriendo',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                    fontSize: 25.0,
+                  ),
+                ),
+                SizedBox(height: 30.0,),
+                Text(
+                  'YEAR',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.black,
+                    letterSpacing: 2.0,
+
+                  ),
+
+                ),
+                Text(
+                  'Third',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                    fontSize: 25.0,
+                  ),
+                ),
+                SizedBox(height: 30.0,),
+                Text(
+                  'EMAIL',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.black,
+                    letterSpacing: 2.0,
+
+                  ),
+
+                ),
+                Text(
+                  'oriendovalencia@gmail.com',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                    fontSize: 25.0,
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                ElevatedButton(
+                    onPressed: (){},
+                    child: Text('Add year',
+                    style: TextStyle(color:Colors.black),
+                    ),
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStateProperty.all(Colors.brown[300])
+                    ),
+                    ),
+              ],
+            ),
+          ],
+        ),
+      ),
+
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,78 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:burgheran/home.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    backgroundColor: Colors.white,
-    appBar: AppBar(
-      title: Text('USER PROFILE'),
-      backgroundColor: Colors.brown[300],
-      centerTitle: true,
-    ),
-    body: Padding(
-      padding: EdgeInsets.all(20.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children:<Widget>[
-          Text(
-            'NAME',
-            style: TextStyle(
-              fontSize: 20.0,
-              color: Colors.black,
-              letterSpacing: 2.0,
-
-            ),
-
-          ),
-          Text(
-            'Rinalyn V.Oriendo',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-              fontSize: 25.0,
-            ),
-          ),
-          SizedBox(height: 30.0,),
-          Text(
-            'YEAR',
-            style: TextStyle(
-              fontSize: 20.0,
-              color: Colors.black,
-              letterSpacing: 2.0,
-
-            ),
-
-          ),
-          Text(
-            'Third',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-              fontSize: 25.0,
-            ),
-          ),
-          SizedBox(height: 30.0,),
-          Text(
-            'EMAIL',
-            style: TextStyle(
-              fontSize: 20.0,
-              color: Colors.black,
-              letterSpacing: 2.0,
-
-            ),
-
-          ),
-          Text(
-            'oriendovalencia@gmail.com',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-              fontSize: 25.0,
-            ),
-          ),
-        ],
-      ),
-    ),
-
-),
+  home: Home(),
 ));
+
+
 
